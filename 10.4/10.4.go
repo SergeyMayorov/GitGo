cf67ece12fb8426d3a40f7ceedd6bacaeb3d6e27
Sergey Mayorov <s.mayorov@automacon.ru>
2024-06-23 00:25:02 +0300

    10.4

--- 10.4/10.4.go
+++ 10.4/10.4.go
@@ -3,13 +3,16 @@ package main
 import (
 	"fmt"
 
-	v1 "v1"
-	v2 "v2"
+	v1 "v1.0.0"
+	v2 "v1.1.0"
 )
 
 func main() {
 	//https://habr.com/ru/articles/421411/
+	//https://go.dev/doc/modules/gomod-ref
 	//go get github.com/SergeyMayorov/10.4
+	//go get github.com/SergeyMayorov/10.4@v1.0.0
+	//go get github.com/SergeyMayorov/10.4@v1.1.0
 	//go mod init mod
 	//go mod tidy
 	fmt.Println(v1.Hello())
